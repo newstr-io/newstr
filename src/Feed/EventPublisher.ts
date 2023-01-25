45c57fc5ca7f3c726d7c46941f18068f51fe47c5
Liran Cohen <c.liran.c@gmail.com>
2023-01-25 08:37:35 -0500

    remove listeners, set auth function onto system

--- src/Feed/EventPublisher.ts
+++ src/Feed/EventPublisher.ts
@@ -7,8 +7,6 @@ import { RootState } from "State/Store";
 import { HexKey, RawEvent, u256, UserMetadata } from "Nostr";
 import { bech32ToHex } from "Util"
 import { DefaultRelays, HashtagRegex } from "Const";
-import { useEffect } from "react";
-import { NIP42AuthChallenge, NIP42AuthResponse } from "Nostr/Auth";
 
 declare global {
     interface Window {
